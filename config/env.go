22505b1a70402615f9f42df47b0105b99ac39b61
istsh <github3104xniiapm@gmail.com>
2019-10-24 02:45:50 +0900

    Fix: condig

--- config/env.go
+++ config/env.go
@@ -3,22 +3,18 @@ package config
 import "os"
 
 type environment struct {
-	goroot      string
-	gopath      string
-	go111module string
+	goroot     string
+	ownProject string
 }
 
 var env *environment
 
-func init() {
+func Setup(ownProject string) {
 	goroot := os.Getenv("GOROOT")
-	gopath := os.Getenv("GOPATH")
-	go111module := os.Getenv("GO111MODULE")
 
 	env = &environment{
-		goroot:      goroot,
-		gopath:      gopath,
-		go111module: go111module,
+		goroot:     goroot,
+		ownProject: ownProject,
 	}
 }
 
@@ -30,10 +26,6 @@ func (e *environment) GetGoRoot() string {
 	return e.goroot
 }
 
-func (e *environment) GetGoPath() string {
-	return e.gopath
-}
-
-func (e *environment) GetGo111Module() string {
-	return e.go111module
+func (e *environment) GetOwnProject() string {
+	return e.ownProject
 }
