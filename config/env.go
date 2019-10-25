a3544e3bef329ae4eb789b17fa22cfa0305eb5ec
istsh <github3104xniiapm@gmail.com>
2019-10-26 00:41:11 +0900

    Fix: env

--- config/env.go
+++ config/env.go
@@ -1,19 +1,37 @@
 package config
 
-import "os"
+import (
+	"flag"
+	"os"
+)
 
 type environment struct {
 	goroot     string
+	filePath   string
 	ownProject string
 }
 
 var env *environment
 
-func Setup(ownProject string) {
+func init() {
+	filePathPtr := flag.String("filepath", "", "file path")
+	ownProjectPtr := flag.String("ownproject", "", "own project")
+	flag.Parse()
+
+	filePath := *filePathPtr
+	if filePath == "" {
+		panic("file path not found")
+	}
+	ownProject := *ownProjectPtr
+	if ownProject == "" {
+		panic("own project not found")
+	}
+
 	goroot := os.Getenv("GOROOT")
 
 	env = &environment{
 		goroot:     goroot,
+		filePath:   filePath,
 		ownProject: ownProject,
 	}
 }
@@ -24,6 +42,10 @@ func GetEnv() *environment {
 
 func (e *environment) GetGoRoot() string {
 	return e.goroot
+}
+
+func (e *environment) GetFilePath() string {
+	return e.filePath
 }
 
 func (e *environment) GetOwnProject() string {
