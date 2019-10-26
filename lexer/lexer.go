28bc416bfad563ea702567ba0c817944ad02cf18
istsh <github3104xniiapm@gmail.com>
2019-10-27 02:45:34 +0900

    WIP: refactoring lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,36 +1,43 @@
 package lexer
 
 import (
-	"strings"
+	"fmt"
 
 	"github.com/istsh/goimport-fmt/ast"
 )
 
-func Lexer(paths []string) ([]*ast.ImportDetail, error) {
+func Lexer(paths []byte) ([]*ast.ImportDetail, error) {
 	var ids []*ast.ImportDetail
 
 	for _, path := range paths {
-		if path == "" || path == "\t" {
-			continue
-		}
-
-		trimStr := strings.Trim(path, "\t")
-		replaceStr := strings.Replace(trimStr, "\"", "", -1)
-		splitStrs := strings.Split(replaceStr, " ")
-
-		if len(splitStrs) == 2 {
-			id, err := ast.AnalyzeIncludeAlias(splitStrs[0], splitStrs[1])
-			if err != nil {
-				return nil, err
-			}
-			ids = append(ids, id)
+		if path == '\n' {
+			fmt.Println()
 		} else {
-			id, err := ast.Analyze(splitStrs[0])
-			if err != nil {
-				return nil, err
-			}
-			ids = append(ids, id)
+			fmt.Print(string(path))
 		}
+
+		//
+		//	//if path == "" || path == "\t" {
+		//	//	continue
+		//	//}
+		//	//
+		//	//trimStr := strings.Trim(path, "\t")
+		//	//replaceStr := strings.Replace(trimStr, "\"", "", -1)
+		//	//splitStrs := strings.Split(replaceStr, " ")
+		//	//
+		//	//if len(splitStrs) == 2 {
+		//	//	id, err := ast.AnalyzeIncludeAlias(splitStrs[0], splitStrs[1])
+		//	//	if err != nil {
+		//	//		return nil, err
+		//	//	}
+		//	//	ids = append(ids, id)
+		//	//} else {
+		//	//	id, err := ast.Analyze(splitStrs[0])
+		//	//	if err != nil {
+		//	//		return nil, err
+		//	//	}
+		//	//	ids = append(ids, id)
+		//	//}
 	}
 
 	return ids, nil
